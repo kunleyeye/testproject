59e69aa041179539ca37cfb49034650aef585832
kunleyeye <47386936+kunleyeye@users.noreply.github.com>
2019-03-05 15:57:07 +0100

    Update Program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -20,6 +20,6 @@ namespace WebApplication1
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();
+                .Build()
     }
 }
