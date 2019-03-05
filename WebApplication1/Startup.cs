d751621737dc9228352d4b6f779c72dfae6a33b3
kunleyeye <47386936+kunleyeye@users.noreply.github.com>
2019-03-05 16:00:01 +0100

    Update Startup.cs

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace WebApplication
+namespace WebApplication1
 {
     public class Startup
     {
